625163f103fc803c5b7856ccf54b7b0faee43b6d
Ryan Filler - Contractor <ryan.filler.contractor@stjude.org>
2018-08-21 14:23:13 -0500

    sets up janky css-transitions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { injectGlobal } from 'styled-components';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import Dinosaur from './dinosaur';
 import Nav from './navigation';
@@ -12,6 +13,37 @@ injectGlobal`
   body {
       margin: 0;
       padding: 0;
+
+      * {
+        transition: 1s;
+      }
+  }
+
+  .fade-enter {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  
+  .fade-enter.fade-enter-active {
+    opacity: 1;
+    transform: translateX(0)
+  }
+
+  .fade-exit {
+    opacity: 1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translateX(0)
+  }
+
+  .fade-exit.fade-exit-active {
+    opacity: 0;
+    transform: translateX(100%)
+  }
+
+  img {
+    width: 300px;
   }
 `
 
@@ -20,20 +52,37 @@ class App extends Component {
   render() {
 
     return (
-      <BrowserRouter>
-          <div>
-            <Nav dinosaurs={dinosaurs} />
+      <Router>
+        <Route
+          render={({ location }) => (
+            <div>
+              <Route
+                exact
+                path="/"
+                render={() => <Redirect to={dinosaurs[0]} />}
+              />
 
-            {dinosaurs.map( (dinosaur, index) => {
-              return (
-                <Route path={`/${dinosaur}`} key={index}
-                  render={() => <Dinosaur dinosaurName={dinosaur} />}
-                />
-              )
-            })}
+              <Nav dinosaurs={dinosaurs} />
 
-          </div>
-        </BrowserRouter>
+              <div style={{position: 'relative'}}>
+                <TransitionGroup>
+                  <CSSTransition key={location.key} classNames="fade" timeout={300}>
+                    <Switch location={location}>
+                      {dinosaurs.map( (dinosaur, index) => {
+                        return (
+                          <Route path={`/${dinosaur}`} key={index}
+                            render={() => <Dinosaur dinosaurName={dinosaur} />}
+                          />
+                        )
+                      })}
+                    </Switch>
+                  </CSSTransition>
+                </TransitionGroup>
+              </div>
+            </div>
+          )}
+        />
+      </Router>
     );
   }
 }
--- src/components/dinosaur.js
+++ src/components/dinosaur.js
@@ -1,16 +1,4 @@
 import React, { Component } from 'react';
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import styled from 'styled-components';
-
-import { styles } from '../config/styles'
-
-const Styles = styled.div`
-    background-color: red;
-
-    .dinosaur {
-        background: blue;
-    }
-`
 
 class Dinosaur extends Component {
     render() {
@@ -18,16 +6,7 @@ class Dinosaur extends Component {
         const {dinosaurName} = this.props;
 
         return (
-            <Styles>
-                <TransitionGroup >
-                <CSSTransition 
-                    classNames="dinosaur"
-                    timeout={500}
-                >
-                    <img src={require(`../images/${dinosaurName}.png`)} alt={dinosaurName}/>
-                </CSSTransition>
-                </TransitionGroup >
-            </Styles>
+            <img src={require(`../images/${dinosaurName}.png`)} alt={dinosaurName}/>
         )
     }
 }
