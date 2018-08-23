5ccbc145ec09f989a6c992ef17318e5e35c3f3d4
Ryan Filler <=>
2018-08-22 19:40:05 -0500

    adds camera

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled, { injectGlobal } from 'styled-components';
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
 import { BrowserView, MobileView, isBrowser, isMobile } from "react-device-detect";
+import Camera from 'react-camera';
 
 import { dinosaurs } from '../config/dinosaurs'
 import { styles } from '../config/styles'
@@ -54,6 +55,16 @@ export default class App extends Component {
 		this.state = {
 			dinosaur: localDinsoaur,
 		};
+
+		this.takePicture = this.takePicture.bind(this);
+	}
+
+	takePicture() {
+		this.camera.capture()
+		.then(blob => {
+			this.img.src = URL.createObjectURL(blob);
+			this.img.onload = () => { URL.revokeObjectURL(this.src); }
+		})
 	}
 
 	componentDidMount() {
@@ -76,6 +87,25 @@ export default class App extends Component {
 												exact path="/"
 												render={() => <Redirect to={this.state.dinosaur ? this.state.dinosaur : dinosaurs[0]} />}
 											/>
+
+											<div style={style.container}>
+													<Camera
+														style={style.preview}
+														ref={(cam) => {
+															this.camera = cam;
+														}}
+													>
+														<div style={style.captureContainer} onClick={this.takePicture}>
+															<div style={style.captureButton} />
+														</div>
+													</Camera>
+														<img
+														style={style.captureImage}
+														ref={(img) => {
+															this.img = img;
+														}}
+													/>
+											</div>
 		
 											<Dinosaurs
 												dinosaurs={dinosaurs}
@@ -94,3 +124,29 @@ export default class App extends Component {
 		);
 	}
 }
+
+const style = {
+	preview: {
+	  position: 'absolute',
+	  top: '0', right: '0', bottom: '0', left: '0'
+	},
+	captureContainer: {
+	  display: 'flex',
+	  position: 'absolute',
+	  justifyContent: 'center',
+	  zIndex: 1,
+	  bottom: 0,
+	  width: '100%',
+	},
+	captureButton: {
+	  backgroundColor: '#fff',
+	  borderRadius: '50%',
+	  height: 56,
+	  width: 56,
+	  color: '#000',
+	  margin: 20
+	},
+	captureImage: {
+	  width: '100%',
+	}
+  };
